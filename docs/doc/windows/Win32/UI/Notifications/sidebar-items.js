0aa9b8080980f63dc3d4069f650a3e67817014f8
Kenny Kerr <kenny@kennykerr.ca>
2022-02-24 13:11:08 -0800

    0.33.0

--- docs/doc/windows/Win32/UI/Notifications/sidebar-items.js
+++ docs/doc/windows/Win32/UI/Notifications/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["INotificationActivationCallback","Required features: ‘Win32_UI_Notifications’"],["NOTIFICATION_USER_INPUT_DATA","Required features: ‘Win32_UI_Notifications’, ‘Win32_Foundation’"]],"trait":[["INotificationActivationCallback_Impl",""]]});
+initSidebarItems({"struct":[["INotificationActivationCallback","Required features: ‘Win32_UI_Notifications’"],["NOTIFICATION_USER_INPUT_DATA","Required features: ‘Win32_UI_Notifications’"]],"trait":[["INotificationActivationCallback_Impl",""]]});
