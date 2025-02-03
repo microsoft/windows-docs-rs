b72f395d270a72bf0098513675f913d74ab604a0
Rafael Rivera <rafael@withinwindows.com>
2025-02-03 13:56:24 -0800

    0.61.0

--- docs/doc/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ docs/doc/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["windows",[["impl&lt;T: RuntimeType&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"windows/Foundation/Collections/struct.IIterator.html\" title=\"struct windows::Foundation::Collections::IIterator\">IIterator</a>&lt;T&gt;"],["impl&lt;T: RuntimeType&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"windows/Foundation/Collections/struct.VectorIterator.html\" title=\"struct windows::Foundation::Collections::VectorIterator\">VectorIterator</a>&lt;T&gt;"],["impl&lt;T: RuntimeType&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"windows/Foundation/Collections/struct.VectorViewIterator.html\" title=\"struct windows::Foundation::Collections::VectorViewIterator\">VectorViewIterator</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["windows",[["impl&lt;T: RuntimeType&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"windows/Foundation/Collections/struct.IIterator.html\" title=\"struct windows::Foundation::Collections::IIterator\">IIterator</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[1186]}
+//{"start":57,"fragment_lengths":[390]}
---
+++ docs/doc/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["windows",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"windows/Win32/System/Com/StructuredStorage/struct.PROPVARIANT.html\" title=\"struct windows::Win32::System::Com::StructuredStorage::PROPVARIANT\">PROPVARIANT</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"windows/Win32/System/Variant/struct.VARIANT.html\" title=\"struct windows::Win32::System::Variant::VARIANT\">VARIANT</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[654]}
