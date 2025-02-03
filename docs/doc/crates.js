0c237de60f336ce11ee2fe1084211ecd8d41ae6b
Rafael Rivera <rafael@withinwindows.com>
2025-02-03 12:39:12 -0800

    0.59.0

--- docs/doc/crates.js
+++ docs/doc/crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["windows"];
+//{"start":21,"fragment_lengths":[9]}
--- docs/doc/trait.impl/windows_core/param/trait.Param.js
+++ docs/doc/trait.impl/windows_core/param/trait.Param.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"windows":[["impl Param&lt;<a class=\"struct\" href=\"windows/Win32/Foundation/struct.BOOL.html\" title=\"struct windows::Win32::Foundation::BOOL\">BOOL</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.bool.html\">bool</a>"],["impl Param&lt;<a class=\"struct\" href=\"windows/Win32/Foundation/struct.BOOLEAN.html\" title=\"struct windows::Win32::Foundation::BOOLEAN\">BOOLEAN</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.bool.html\">bool</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["windows",[["impl Param&lt;<a class=\"struct\" href=\"windows/Win32/Foundation/struct.BOOL.html\" title=\"struct windows::Win32::Foundation::BOOL\">BOOL</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.bool.html\">bool</a>"],["impl Param&lt;<a class=\"struct\" href=\"windows/Win32/Foundation/struct.BOOLEAN.html\" title=\"struct windows::Win32::Foundation::BOOLEAN\">BOOLEAN</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.bool.html\">bool</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[532]}
